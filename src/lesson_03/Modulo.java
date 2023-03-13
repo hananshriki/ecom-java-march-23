8c384cd7c0c53efcf5411a0f1759fb3fc06b018a
hananshriki <hanan1234568@gmail.com>
2023-03-13 18:53:53 +0200

    modulo

---
+++ src/lesson_03/Modulo.java
@@ -0,0 +1,14 @@
+package lesson_03;
+
+public class Modulo {
+    public static void main(String[] args) {
+
+        int a = 3687;
+
+
+        System.out.println(a + " % " + 10 + " = " + (a%10));
+        System.out.println(a + " % " + 100 + " = " + (a%100));
+        System.out.println(a + " / " + 10 + " = " + (a/10));
+        System.out.println(a + " / " + 100 + " = " + (a/100));
+    }
+}
---
+++ src/lesson_03/Operators.java
@@ -0,0 +1,21 @@
+package lesson_03;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 6;
+
+        double c = 20;
+        double d = 6;
+
+        System.out.println(a + " + " + b + " = " + (a+b));
+        System.out.println(3 + " ** " + 4 + " = " + (Math.pow(3,4)));
+        System.out.println(a + " - " + b + " = " + (a-b));
+        System.out.println(a + " * " + b + " = " + (a*b));
+        System.out.println(a + " / " + b + " = " + (a/b));
+        System.out.println(c + " / " + d + " = " + (c/d));
+        System.out.println(a + " % " + b + " = " + (a%b));
+        System.out.println(18 + " % " + b + " = " + (18 %b));
+        System.out.println(c + " / " + 0 + " = " + (c/0));}
+    }
