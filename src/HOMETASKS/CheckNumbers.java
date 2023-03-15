e0ba043074268b1ded52dc35dc91da20973e30e2
hananshriki <hanan1234568@gmail.com>
2023-03-15 20:58:41 +0200

    home task

---
+++ src/HOMETASKS/CheckNumbers.java
@@ -0,0 +1,26 @@
+package HOMETASKS;
+
+import java.util.Scanner;
+
+public class CheckNumbers {
+    public static void main(String[] args) {
+
+        int a = 10 % 2;
+        int b = 25 % 2;
+        int c = 55 % 2;
+        int d = 22 % 2;
+        int e = 0 % 2;
+
+        System.out.println(a + " == " + 0 + " -> " + (a == 0));
+
+        System.out.println(b + " == " + 0 + " -> " + (b == 0));
+
+        System.out.println(c + " == " + 0 + " -> " + (c == 0));
+
+        System.out.println(d + " == " + 0 + " -> " + (d == 0));
+
+        System.out.println(e + " == " + 0 + " -> " + (e == 0));
+
+
+    }
+}
